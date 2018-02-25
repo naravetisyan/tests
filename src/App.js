f07bfda0c845c0018c6d09bbd350a9ab63a0314a
narekavetsiyan <nnnavetisyan@gmail.com>
2018-02-25 21:13:20 +0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
+    let arr = [1, 2, 3, 4, 5, 6];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <i className="am flag" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <select>
+          {
+          arr.map((value, index) => {
+            return <option key={index}>{value}</option>;
+          })}
+        </select>
       </div>
     );
   }
